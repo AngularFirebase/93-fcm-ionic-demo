084d1c5d5d9abb43c77e18d453188fed4bf4382f
codediodeio <hello@codediode.io>
2018-03-10 09:44:34 -0700

    functions config

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FcmProvider } from '../../providers/fcm/fcm'
 
 @Component({
   selector: 'page-home',
@@ -7,10 +8,10 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {}
+  constructor(public navCtrl: NavController, public fcm: FcmProvider) {}
 
   ionViewDidLoad(){
-
+    this.fcm.getToken()
   }
   
   
