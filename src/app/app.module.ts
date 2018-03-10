d2610cf15127653b0142bca21422c46a866eab92
codediodeio <hello@codediode.io>
2018-03-10 14:50:47 -0700

    rm config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,12 +18,7 @@ import { AngularFireStorageModule } from 'angularfire2/storage';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 
 const firebase = {
-  apiKey: "AIzaSyAc9Tt8pQQF3ntu2Tz66iUKl_fXHtdhIyc",
-  authDomain: "firestarter-96e46.firebaseapp.com",
-  databaseURL: "https://firestarter-96e46.firebaseio.com",
-  projectId: "firestarter-96e46",
-  storageBucket: "firestarter-96e46.appspot.com",
-  messagingSenderId: "581326886241"
+ // your firebase web config
 }
 
 import { Firebase } from '@ionic-native/firebase';
